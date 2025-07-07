91c63d4fd90a1ba69bb9fd9399b278918445ced4
JoaoGomes8 <joaogomes2609@gmail.com>
2025-07-07 20:03:05 +0100

    strict mode

---
+++ 005_strict_mode/script_1.js
@@ -0,0 +1,10 @@
+// strict mode - é uma forma de indicar ao Javascript que quero
+// usar um modo mais rigoroso de execução. Ele ajuda a evitar erros comuns e
+// melhora a segurança do código. Quando ativa o modo estrito,
+// algumas práticas ruins são desencorajadas e algumas práticas permitidas são desativadas
+
+"use strict";
+
+let firstName = "Steve"  // válido
+
+lastName = "Jobs"        // inválido. É válido se não tiver o strict mode
